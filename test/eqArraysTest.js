77812a0e1fdcffae032a2f90d6c5726cd02d1781
Bethany Paul <123354450+Bethanypaul989@users.noreply.github.com>
2023-03-13 22:38:05 -0600

    refactored eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,19 @@
+const eqArrays = require('../eqArrays');
+
+describe("#eqArrays", () => {
+  it("returns true if arrays are equal", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns false if arrays are not equal", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  it("returns false if arrays are not equal", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+
+  it("returns true if arrays are equal", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+});
