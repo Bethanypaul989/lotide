6099507ecb17cb04f476797ddaba00af266cf2bb
Bethany Paul <123354450+Bethanypaul989@users.noreply.github.com>
2023-03-13 22:33:50 -0600

    refactored

---
+++ test/tailTest.js
@@ -0,0 +1,19 @@
+const tail = require('../tail');
+
+describe("#tail", () => {
+  it("returns ['Is', 'Fun'] for ['Coding', 'Is', 'Fun']", () => {
+    assert.deepEqual(tail(["Coding", "Is", "Fun"]), ["Is", "Fun"]);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
+
+  it("returns an [] for its tail if passed an array with one element", () => {
+    assert.deepEqual(tail(["X"]), []);
+  });
+
+  it("returns an empty array for its tail if passed an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
